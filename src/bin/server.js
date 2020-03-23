a7d8071c605ce2fa03a49e8d0abe1c67e0278c23
danylokarpenko <karpenkodanylo@gmail.com>
2020-03-23 22:46:43 +0200

    Solved deploying config problems

--- src/bin/server.js
+++ src/bin/server.js
@@ -3,5 +3,7 @@ import runServer from '../';
 const port = 8080;
 
 runServer().listen(port, () => {
+  console.log(runServer);
+
   console.log(`Server has been started on port: ${port}`);
 })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // include and initialize the rollbar library with your access token
 import Koa from 'koa';
 import Rollbar from 'rollbar';
+
 const rollbar = new Rollbar({
   accessToken: '2a409f450d4640f6a004fdd38a1e12dd',
   captureUncaught: true,
@@ -11,11 +12,11 @@ rollbar.log("Application!");
 
 export default () => {
   const app = new Koa();
+
   app.use(async (ctx, next) => {
     try {
       await next();
     } catch (err) {
-      // will only respond with JSON
       ctx.status = err.statusCode || err.status || 500;
       ctx.body = {
         message: err.message
@@ -25,10 +26,8 @@ export default () => {
   })
 
   app.use(async (ctx, next) => {
-    ctx.body = 'Dark power';
+    ctx.body = 'Dark power!!';
     await next();
   })
-
   return app;
 }
-// record a generic message and send it to Rollbar
